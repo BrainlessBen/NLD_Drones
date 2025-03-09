65d41da55c44db3f5825699338899dc7926f7bdb
Ben van Loon <Bennie_vloon@outlook.com>
2025-03-09 12:52:44 +0100

    Update Defines.hpp

--- Defines.hpp
+++ Defines.hpp
@@ -9,27 +9,79 @@
 #define BEN_SubCatCrocus "BEN_D_Crocus"
 #define BEN_SubCatRaven "BEN_D_Raven"
 
-//Drone settings
-#define DRONE_Accuracy 1.5
-#define DRONE_Camoflage 0.5
-#define DRONE_Audible 0.1
-#define DRONE_Armor 0.5
-#define DRONE_Cost 20000
-#define DRONE_AltFullForce 500
-#define DRONE_AltNoForce 1000
-#define DRONE_StartDuration 1
-#define DRONE_MaxSpeed 100
-#define DRONE_Precision 15
-#define DRONE_SteerAheadSimul 0.2
-#define DRONE_SteerAheadPlan 0.6
-#define DRONE_PredictTurnSimul 2
-#define DRONE_PreductTurnPlan 3
-#define DRONE_LiftForce 7
-#define DRONE_CyclicAsideForce 2
-#define DRONE_CyclicForwardForce 1
-#define DRONE_BodyFriction 0
-#define DRONE_BackRotorForce 2
-#define DRONE_FuelCap 5
-#define DRONE_MainRotorSpeed -10
-#define DRONE_BackRotorSpeed 10
-#define DRONE_TailBladeVertical 0
+/*
+default settings
+    accuracy=1.5;
+    camouflage=0.5;
+    audible=0.1;
+    armor=0.5;
+    cost=20000;
+    altFullForce=1000;
+    altNoForce=2000;
+    startDuration=3;
+    maxSpeed=100;
+    precision=15;
+    steerAheadSimul=0.5;
+    steerAheadPlan=0.69999999;
+    predictTurnPlan=2;
+    predictTurnSimul=1.5;
+    liftForceCoef=1;
+    cyclicAsideForceCoef=2;
+    cyclicForwardForceCoef=1.2;
+    bodyFrictionCoef=0.30000001;
+    backRotorForceCoef=5;
+    fuelCapacity=100;
+    mainRotorSpeed=-7;
+    backRotorSpeed=7;
+    tailBladeVertical=0;
+*/
+
+//Drone1 settings
+#define Drone1_Accuracy 1.5
+#define Drone1_Camoflage 0.5
+#define Drone1_Audible 0.1
+#define Drone1_Armor 0.5
+#define Drone1_Cost 2000
+#define Drone1_AltFullForce 250
+#define Drone1_AltNoForce 500
+#define Drone1_StartDuration 2
+#define Drone1_MaxSpeed 50
+#define Drone1_Precision 15
+#define Drone1_SteerAheadSimul 0.4
+#define Drone1_SteerAheadPlan 0.7
+#define Drone1_PredictTurnSimul 1
+#define Drone1_PreductTurnPlan 1
+#define Drone1_LiftForce 5
+#define Drone1_CyclicAsideForce 1
+#define Drone1_CyclicForwardForce 1
+#define Drone1_BodyFriction 0.30000001
+#define Drone1_BackRotorForce 5
+#define Drone1_FuelCap 10
+#define Drone1_MainRotorSpeed -7
+#define Drone1_BackRotorSpeed 7
+#define Drone1_TailBladeVertical 0
+
+//Drone2 settings
+#define Drone2_Accuracy 1.5
+#define Drone2_Camoflage 0.5
+#define Drone2_Audible 0.1
+#define Drone2_Armor 0.5
+#define Drone2_Cost 2000
+#define Drone2_AltFullForce 500
+#define Drone2_AltNoForce 1000
+#define Drone2_StartDuration 1
+#define Drone2_MaxSpeed 100
+#define Drone2_Precision 15
+#define Drone2_SteerAheadSimul 0.2
+#define Drone2_SteerAheadPlan 0.6
+#define Drone2_PredictTurnSimul 2
+#define Drone2_PreductTurnPlan 3
+#define Drone2_LiftForce 7
+#define Drone2_CyclicAsideForce 2
+#define Drone2_CyclicForwardForce 1
+#define Drone2_BodyFriction 0
+#define Drone2_BackRotorForce 2
+#define Drone2_FuelCap 5
+#define Drone2_MainRotorSpeed -10
+#define Drone2_BackRotorSpeed 10
+#define Drone2_TailBladeVertical 0
